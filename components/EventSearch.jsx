e384f4bf10cd0484c24a41bacfe134083249994c
Abhishek <abhishekparmar162@gmail.com>
2022-11-02 00:58:26 +0530

    added functionality to search button

--- components/EventSearch.jsx
+++ components/EventSearch.jsx
@@ -1,14 +1,38 @@
 import React from 'react'
+import { useRef } from 'react'
+import { useRouter } from 'next/router';
 
 const EventSearch = () => {
+const router = useRouter()
+ const YearRef= useRef();
+ const MonthRef= useRef();
+
+ const onSearch =(year,month) => {
+    const fullPath = `/events/${year}/${month}`
+        router.push(fullPath)
+    
+     }
+
+ 
+ const searchHandler = (e) => {
+        e.preventDefault()
+ const selectedYear = YearRef.current.value;
+ const selectedMonth = MonthRef.current.value;
+
+ 
+
+       onSearch(selectedYear, selectedMonth)
+    }
+
+
   return (
-    <form action="">
+    <form onSubmit={searchHandler}>
         <div className='flex item-center justify-center '>
             
 <div className='pr-4 pt-4 '>
 
         <label htmlFor="year">Year</label>
-        <select id="year">
+        <select id="year" ref={YearRef}>
             <option value="2022">2022</option>
             <option value="2021">2021</option>
             <option value="2020">2020</option>
@@ -16,7 +40,7 @@ const EventSearch = () => {
 </div>
 <div className='pr-4 pt-4 '>
         <label htmlFor="month">Month</label>
-        <select id="month">
+        <select id="month" ref={MonthRef}>
         <option value="01">January</option>
         <option value="02">February</option>
         <option value="03">March</option>
@@ -31,14 +55,14 @@ const EventSearch = () => {
         <option value="12">December</option>
         </select>
         </div>
-<a href="#_" class="relative ml-4 pt-4 inline-block text-lg group">
+<button  className="relative ml-4 pt-4 inline-block text-lg group">
 <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
 <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
 <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
 <span className="relative">Search Events</span>
 </span>
 <span className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0" data-rounded="rounded-lg"></span>
-</a>
+</button>
 </div>
 
     </form>
