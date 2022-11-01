0f628dbb7a16bfbffb0b4615b9fe62b3ef3c993d
Abhishek <abhishekparmar162@gmail.com>
2022-11-01 17:02:18 +0530

    fix home page

--- components/Event/EventItem.jsx
+++ components/Event/EventItem.jsx
@@ -14,23 +14,30 @@ const EventItem = (props) => {
       const exploreLink = `/events/${id}`;
 
   return (
-    <div className='flex flex-col item-center  p-40 justify-center'>
-      <div className='w-96 h-64'>
-
-         <Image src={image} width='240' height='240' />
-      </div>
-        <div>
-            <div>
-                <h2>{title}</h2>
-                <time>{humanReadableDate}</time>
-                <address>{formattedAddress}</address>
-            </div>
-            <Link href={exploreLink}>
-              <button className='bg-blue-500 p-2 rounded-full text-white'>Explore Event</button>
-            </Link>
+    <section className="text-gray-600 body-font overflow-hidden">
+  <div className="container px-5 py-24 mx-auto">
+    <div className="lg:w-4/5 mx-auto flex flex-wrap">
+      <div className="lg:w-1/2 w-full lg:pr-10 lg:py-6 mb-6 lg:mb-0">
+        <h1 className="text-gray-900 text-3xl title-font font-medium mb-4">{title}</h1>
+        <div className="flex border-t border-gray-200 py-2">
+          <span className="text-gray-500">Size</span>
+          <span className="ml-auto text-gray-900">Medium</span>
         </div>
-        
+        <div className="flex border-t border-b mb-6 border-gray-200 py-2">
+          <span className="text-gray-500">{humanReadableDate}</span>
+        </div>
+        <div className="flex">
+          <span className="title-font font-medium text-2xl text-gray-900">{formattedAddress}</span>
+          <button className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Button</button>
+          
+        </div>
+      </div>
+      <img alt="ecommerce" className="lg:w-1/2 w-96 lg:h-auto h-64 object-cover object-center rounded" src={image} />
     </div>
+  </div>
+</section>
+
+
   )
 }
 
