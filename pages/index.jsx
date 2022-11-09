a5e19123f4870deb7aa8b0ee3476c78cf05ff31a
Abhishek <abhishekparmar162@gmail.com>
2022-11-09 23:33:26 +0530

    display home page from firebase

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,22 @@
-import {getFeaturedEvents} from '../dummy_data'
+import {getFeaturedEvents} from '../helper/helper'
 import EventList from '../components/Event/EventList'
 
-const index = () => {
-  const featuredEvent = getFeaturedEvents()
+const index = (props) => {
+ 
   return (
     <div>
-<EventList items={featuredEvent} />      
+<EventList items={props.event} />      
     </div>
   )
 }
 
-export default index
+export default index;
+
+export async function getStaticProps() {
+  const featuredEvent = await getFeaturedEvents();
+  return {
+    props: {
+      event : featuredEvent,
+    }, // will be passed to the page component as props
+  }
+}
